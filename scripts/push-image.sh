19fb3e8bdc634a7320c4b19ee1f1ca77864c9f07
Mario Vitale <mvitale1989@hotmail.com>
2023-01-26 22:15:29 +0100

    Fix forgotten GITHUB_REF

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -3,18 +3,18 @@ set -e
 set -u
 
 : ${GITHUB_REF_TYPE:?}
-: ${GITHUB_REF:?}
+: ${GITHUB_REF_NAME:?}
 
 #env >> BASH_ENV
 #cat BASH_ENV | while read line; do
 #	export $line
 #done
 if [ "${GITHUB_REF_TYPE:-}" = "branch" ]; then
-  echo "Pushing latest for $GITHUB_REF..."
+  echo "Pushing latest for $GITHUB_REF_NAME..."
   export TAG=latest
 else
-  echo "Pushing release $GITHUB_REF..."
-  export TAG="$GITHUB_REF"
+  echo "Pushing release $GITHUB_REF_NAME..."
+  export TAG="$GITHUB_REF_NAME"
 fi
 make build-image-with-tag
 
